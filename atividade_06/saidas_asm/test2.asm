d439ac2edf4394a85018f98de36817b336d6a079
Lucas <lucasfm.py@gmail.com>
2025-03-21 14:06:54 -0300

    mudando caso teste

--- atividade_06/saidas_asm/test2.asm
+++ atividade_06/saidas_asm/test2.asm
@@ -3,6 +3,8 @@
 
 .section .text
 _start:
+mov $4, %rax
+push %rax
 mov $5, %rax
 push %rax
 mov $4, %rax
@@ -12,6 +14,9 @@ push %rax
 mov $3, %rax
 pop %rbx
 imul %rbx, %rax
+pop %rbx
+mov $0, %rdx
+div %rbx
 call imprime_num
 call sair
 .include "runtime.s"
